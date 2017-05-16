32991eba4f2fd24d2635893f8c24d764cc49511d
Juan Cruz Basso <juan.basso@mercadolibre.com>
2017-05-16 19:33:17 -0300

    Modifico graficador para que este mas lindo

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ reader = csv.reader(open("S1.csv"), delimiter=",")
 ips = []
 g = nx.Graph()
 
-
+aPartirDeQueCantidadMostrarLabels = 35
 timesAsking = {"10.10.90.23" : 3,
 "10.10.90.91" : 4,
 "10.10.91.114" : 8,
@@ -26,32 +26,10 @@ timesAsking = {"10.10.90.23" : 3,
 "10.10.91.23" : 1,
 "10.10.90.114" : 47}
 
-timesBeingSearch = {"10.10.91.16" : 276,
-"10.10.90.40" : 18,
-"10.10.90.215" : 18,
-"10.10.90.15" : 46,
-"10.10.91.56" : 2,
-"10.10.91.32" : 64,
-"10.10.90.23" : 19,
-"10.10.91.75" : 4,
-"10.10.88.71" : 9,
-"10.10.91.9": 47,
-"10.10.90.0": 17,
-"10.10.91.132" : 19,
-"169.254.255.255" : 5,
-"10.10.91.114" : 25,
-"10.10.90.91" : 6,
-"10.10.90.225" : 17,
-"10.10.90.55" : 6,
-"10.10.12.1" : 4,
-"10.10.90.19" : 13,
-"10.10.91.183" : 8,
-"10.10.88.64" : 3,
-"10.10.91.23" : 1,
-"10.10.90.80" : 3,
-"10.10.88.1": 98,
-"10.10.90.114" : 15,
-"10.200.1.51" : 6}
+labels = dict()
+for key in timesAsking.keys():
+	if(timesAsking[key] >= aPartirDeQueCantidadMostrarLabels):
+		labels[key] = key
 
 for ipFrom, ipSearchingFor in reader:
 	g.add_edge(ipFrom,ipSearchingFor)
@@ -82,7 +60,7 @@ for key in nodeSizesAsking.keys():
 	nodeSizesBeingSearch[key] = (nodeSizesBeingSearch[key] * newMax) / oldMaxBeingSearch
 
 pos = nx.spring_layout(g,scale=1) #default to scale=1
-nx.draw(g,pos, with_labels=True,edge_color="grey",width=0.5,node_size=[v for v in nodeSizesAsking.values()])
+nx.draw(g,pos, labels = labels,node_color = "#97edea",with_labels=True,edge_color="grey",width=0.5,node_size=[v for v in nodeSizesAsking.values()])
 plt.show()
 # nx.draw(g,pos, with_labels=True,node_size=[v for v in nodeSizesBeingSearch.values()])
 # plt.show()
